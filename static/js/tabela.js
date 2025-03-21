e43e19a6cfe8fc03f2b580d24f948ffa7872f1fc
Rafael Lopes Ens <rafaellopes.ifrs@gmail.com>
2025-03-21 09:15:00 -0300

    add filter in objects Documentos

--- static/js/tabela.js
+++ static/js/tabela.js
@@ -12,18 +12,112 @@ async function buscaArquivos() {
     }
 }
 
-async function carregaDados(){
-    dados = await buscaArquivos()
-    console.log(dados)
-    criaTabela(dados)
-}
-
 // variaveis
 let tabela = document.querySelector('#tabela')
+let divFiltrosAtivos = document.querySelector('#filtros-ativos')
+
+
+let filtros_ativos = []
 
 let paginaAtual = 0
 const itensPorPagina = 10
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+//filtros
+function adicionaFiltro() {
+    let nome = document.querySelector('#nome').value;
+    let ano = document.querySelector('#ano').value;
+    let categoria = document.querySelector('#categoria').value
+
+    let anoAtual = new Date().getFullYear()
+
+    console.log(`Nome=${nome} | ano=${ano} | categoria=${categoria}`)
+
+    if (nome.trim() !== '') {
+        filtros_ativos.push({type: 'nome', value: nome})
+    }
+
+    if ( ano.trim() !== '' && Number(ano) <= anoAtual) {
+        filtros_ativos.push({type: 'ano', value: ano})
+    }
+
+    if (categoria.trim() !== ''){
+        filtros_ativos.push({type: 'categoria', value: categoria})
+    }
+
+    document.querySelector('#nome').value = ''
+    document.querySelector('#ano').value = ''
+    document.querySelector('#categoria').value = ''
+
+    exibeFiltrosAtivos()
+    atualizaTabela()
+}
+
+
+function deletaFiltro(id){
+    filtros_ativos.splice(id, 1)
+    exibeFiltrosAtivos()
+    atualizaTabela()
+}
+
+
+function exibeFiltrosAtivos() {
+    
+    if (filtros_ativos && filtros_ativos.length > 0) {
+
+        divFiltrosAtivos.innerHTML = '';
+
+        filtros_ativos.forEach((itemFiltro, index) => {
+            const campoFiltro = document.createElement('div');
+            campoFiltro.classList.add("campo-filtro");
+            campoFiltro.innerHTML = `<p>${itemFiltro.type}</p> <button onclick="deletaFiltro(${index})">X</button>`;
+            divFiltrosAtivos.appendChild(campoFiltro);
+        })
+    } else {
+        divFiltrosAtivos.innerHTML = '<p>Nenhum filtro ativo</p>';
+    }
+}
+
+
+function filtraDados() {
+        
+    let dadosFiltrados = [...dados]
+        
+    filtros_ativos.forEach(element => {
+        if(element.type === 'nome'){
+            dadosFiltrados = dadosFiltrados.filter(item => {return item.Nome.toLocaleLowerCase().includes(element.value.toLocaleLowerCase())})
+        }
+
+        if(element.type === 'ano'){
+            dadosFiltrados = dadosFiltrados.filter(item => {return item.Ano == element.value})
+        }
+
+        if(element.type === 'categoria'){
+            dadosFiltrados = dadosFiltrados.filter(item => {return item.Categoria.toLocaleLowerCase() === element.value.toLocaleLowerCase()})
+        }
+    });
+
+    return dadosFiltrados
+}
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+// tabela
+function criaTabela(dados){
+
+    let totalPaginasSpan = document.querySelector('#total-paginas')
+    let paginaAtualSpan = document.querySelector('#pagina-atual')
+
+    let qtdPaginas = Math.ceil(dados.length / itensPorPagina)
+
+    let inicio = paginaAtual * itensPorPagina
+    let fim = inicio + itensPorPagina
+
+    let dadosPaginaAtual = dados.slice(inicio, fim)
+
+    preencheTabela(dadosPaginaAtual)
+    paginaAtualSpan.innerHTML = `${paginaAtual+1}`
+    totalPaginasSpan.innerHTML = `${qtdPaginas}`
+}
 
 function preencheTabela(dados){
     
@@ -40,6 +134,7 @@ function preencheTabela(dados){
         linha.innerHTML = `
             <td>${element.Nome}</td>
             <td>${element.Ano}</td>
+            <td>${element.Categoria}</td>
             <td>${new Date(element.UpdatedAt).toLocaleString()}</td>
             <td>
                 <button value="${element.ID}" class="btnDownload" onClick="baixaArquivo(${element.ID})">
@@ -59,23 +154,19 @@ function preencheTabela(dados){
     });
 }
 
-function criaTabela(array){
-
-    let totalPaginasSpan = document.querySelector('#total-paginas')
-    let paginaAtualSpan = document.querySelector('#pagina-atual')
-
-    let qtdPaginas = Math.ceil(array.length / itensPorPagina)
-
-    let inicio = paginaAtual * itensPorPagina
-    let fim = inicio + itensPorPagina
-
-    let dadosPaginaAtual = array.slice(inicio, fim)
-
-    preencheTabela(dadosPaginaAtual)
-    paginaAtualSpan.innerHTML = `${paginaAtual+1}`
-    totalPaginasSpan.innerHTML = `${qtdPaginas}`
+function atualizaTabela(){
+    if(filtros_ativos.length > 0){
+        const dadosFiltrados = filtraDados()
+        criaTabela(dadosFiltrados)
+    } else {
+        criaTabela(dados)
+    }
 }
 
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+
+// paginacao
 function voltaDados(){
     if(paginaAtual > 0){
         paginaAtual--;
@@ -89,6 +180,14 @@ function avancaDados(){
         paginaAtual++
         criaTabela(dados)
     }
+}
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+
+async function carregaDados(){
+    dados = await buscaArquivos()
+    atualizaTabela()
 }
 
 carregaDados()
